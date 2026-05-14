a9a386dfbf86e0cde6a6eacac132c0491f1dab34
gartiluigi-blip <gartiluigi@gmail.com>
2026-05-15 01:21:01 +0200

    refactor: add patch-all runner

---
+++ tools/patch-all.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+'use strict';
+
+const { spawnSync } = require('node:child_process');
+
+const patchers = [
+  'tools/patch-index-storage.js',
+  'tools/patch-v73-core.js',
+  'tools/patch-v72-storage.js',
+  'tools/patch-dead-code-v75-focus.js',
+  'tools/patch-epfc-dead-panels.js'
+];
+
+for (const patcher of patchers) {
+  console.log(`\n$ node ${patcher}`);
+  const result = spawnSync(process.execPath, [patcher], { stdio: 'inherit' });
+  if (result.status !== 0) process.exit(result.status || 1);
+}
+
+console.log('\npatch-all OK');
