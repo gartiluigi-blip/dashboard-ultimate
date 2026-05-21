5f07b80e9f7996926761cf9f263e5b4490583606
gartiluigi-blip <gartiluigi@gmail.com>
2026-05-22 01:05:31 +0200

    fix: align module imports with onyx dark version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
-import { $, el } from './ui.js?v=20260520-ux-1';
-import { renderHome } from '../../modules/home.js?v=20260520-ux-1';
-import { renderSport } from '../../modules/sport.js?v=20260520-ux-1';
-import { renderStudy } from '../../modules/study.js?v=20260520-ux-1';
-import { renderRoutine } from '../../modules/routine.js?v=20260520-ux-1';
-import { renderMoney } from '../../modules/money.js?v=20260520-ux-1';
-import { renderLeisure } from '../../modules/leisure.js?v=20260520-ux-1';
-import { renderFuelFab } from '../../modules/fuel.js?v=20260520-ux-1';
-import { renderStats } from '../../modules/stats.js?v=20260520-ux-1';
-import { renderSettings } from '../../modules/settings.js?v=20260520-ux-1';
+import { $, el } from './ui.js?v=20260520-onyx-dark-1';
+import { renderHome } from '../../modules/home.js?v=20260520-onyx-dark-1';
+import { renderSport } from '../../modules/sport.js?v=20260520-onyx-dark-1';
+import { renderStudy } from '../../modules/study.js?v=20260520-onyx-dark-1';
+import { renderRoutine } from '../../modules/routine.js?v=20260520-onyx-dark-1';
+import { renderMoney } from '../../modules/money.js?v=20260520-onyx-dark-1';
+import { renderLeisure } from '../../modules/leisure.js?v=20260520-onyx-dark-1';
+import { renderFuelFab } from '../../modules/fuel.js?v=20260520-onyx-dark-1';
+import { renderStats } from '../../modules/stats.js?v=20260520-onyx-dark-1';
+import { renderSettings } from '../../modules/settings.js?v=20260520-onyx-dark-1';
 const tabs=[['home','Aujourd\'hui'],['routine','Routine'],['sport','Sport'],['study','Étude'],['money','Argent'],['leisure','Loisir'],['stats','Stats'],['settings','Réglages']];let route='home';
 function nav(){const n=$('#tabs');n.innerHTML='';tabs.forEach(([id,l])=>n.append(el('button',{class:'tab '+(route===id?'active':''),onclick:()=>{route=id;render();}},l)));}
 function render(){nav();const app=$('#app');app.innerHTML='';renderFuelFab(render);({home,routine,sport,study,money,leisure,stats,settings}[route]||home)(app);}
