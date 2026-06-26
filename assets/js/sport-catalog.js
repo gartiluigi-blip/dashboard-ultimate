9f822e7a05565b48a3ce7323be8830fbb0c048a4
gartiluigi-blip <gartiluigi@gmail.com>
2026-06-26 19:17:38 +0200

    refactor: use sport catalog as hybrid module index

--- assets/js/sport-catalog.js
+++ assets/js/sport-catalog.js
@@ -1,1 +1,8 @@
-export const SPORT_CATALOG_VERSION='1.0.0';
+import * as Hybrid from './hybrid-ppl-data.js';
+
+export const SPORT_CATALOG_VERSION='hybrid-ppl-v1';
+export const SPORT_CYCLE=Hybrid.SPORT_CYCLE;
+export const SPORT_LIBRARY=Hybrid.SPORT_LIBRARY;
+export const SPORT_PROGRAM=Hybrid.SPORT_PROGRAM;
+export const BODYWEIGHT_LADDERS=Hybrid.BODYWEIGHT_LADDERS;
+export const MUSCLE_LABELS=Hybrid.MUSCLE_LABELS;
