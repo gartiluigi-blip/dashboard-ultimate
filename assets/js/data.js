b9315092a2c198c695f791783747b648e46dfec2
gartiluigi-blip <gartiluigi@gmail.com>
2026-06-26 19:18:01 +0200

    refactor: route dashboard sport exports through catalog index

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,9 +1,9 @@
-import * as HybridSport from './hybrid-ppl-data.js';
+import * as HybridSport from './sport-catalog.js';
 
 export const SPORT_CYCLE=HybridSport.SPORT_CYCLE;
 export const SPORT_LIBRARY=HybridSport.SPORT_LIBRARY;
 export const SPORT_PROGRAM=HybridSport.SPORT_PROGRAM;
-export const FLEX_LEVELS=HybridSport.FLEX_LEVELS;
+export const FLEX_LEVELS=[];
 export const BODYWEIGHT_LADDERS=HybridSport.BODYWEIGHT_LADDERS;
 export const MUSCLE_LABELS=HybridSport.MUSCLE_LABELS;
 
